c87d9815ee88ff5384642ad613eefd042fbb1999
countofkrakow <chrisnakovski@gmail.com>
2016-02-27 02:39:11 -0800

    fixed issues with test

--- Kernels/test.py
+++ Kernels/test.py
@@ -7,19 +7,20 @@ class kernelTest(unittest.TestCase):
         train_data = extract_features(load_train_data())
         test_data = extract_features(load_test_data())
         p = perceptron(train_data)
-        p.train()
+        p.train(50)
+        print p.a
 
         correct = 0
         for point in train_data:
             if p.predict(point):
                 correct += 1
-        print('Training data accuracy %f' % (correct / len(train_data)))
+        print('Training data accuracy %f' % (float(correct) / len(train_data)))
 
         correct = 0
         for point in test_data:
             if p.predict(point):
                 correct += 1
-        print('Test data accuracy %f' % (correct / len(test_data)))
+        print('Test data accuracy %f' % (float(correct) / len(test_data)))
 
 if __name__ == '__main__':
     unittest.main()
