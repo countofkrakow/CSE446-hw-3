dd91dac4eb27a6bea7a25b8d1ab8ee8ba2a01477
countofkrakow <chrisnakovski@gmail.com>
2016-02-27 20:21:07 -0800

    Cleaned up the perceptron code

--- Kernels/kernel.py
+++ Kernels/kernel.py
@@ -1,43 +1,32 @@
-import operator
 
 class perceptron:
-    def __init__(self, data, kern=None, trainingSteps=100):
-        if kern is None:
-            self.kern = self.dot_prod
-        else:
-            self.kern = kern
-        self.steps = trainingSteps
-        self.data = extract_features(data)
+    def __init__(self, data, kern):
+        self.kern = kern
+        self.data = data
 
-    def dot_prod(self, x, y):
-        assert len(x) == len(y)
-        return sum(map(operator.mul, x, y)
-
-    def train(self):
-        lastMistake = 0 # used for exiting early when we converge
+    def train(self, iterations=1000, lossInterval=None):
+        wrong = 0
         self.a = [0]*len(self.data)
-        for i in range(self.steps): # num iterations
-            imod = i % len(self.data)
-            x_i = self.data[imod]['features']
-            y_i = self.data[imod]['label']
+        for i in range(iterations): # num iterations
 
-            if not self.predict(x_i):
-                # we've hit an error
-                lastMistake = imod
+            imod = i % len(self.data)
+            y_i = self.data[imod]['label']
+            if not self.predict(self.data[imod]):
+                wrong += 1
                 if sign(y_i):
                     self.a[imod] += 1
                 else:
                     self.a[imod] -= 1
 
-            elif imod == lastMistake and i != 0: # converged
-                print("Training converged at iteration %d" % i)
-                break
+            if lossInterval is not None and i % lossInterval == 0:
+                print 'average loss at %d steps: %f' % (i, float(wrong)/(i+1))
 
     def predict(self, point):
         pred = 0
         for j in range(len(self.data)):
             pred += self.a[j] * self.kern(point['features'], self.data[j]['features'])
-        return sign(pred) == sign(point['label'])
+        ret = sign(pred) == sign(point['label'])
+        return ret
 
 # True for x > 0
 # False for x <= 0
@@ -48,10 +37,10 @@ def extract_features(raw):
     data = []
     for r in raw:
         point = {}
-        point["label"] = (r['valy'])
+        point["label"] = int(r['label'])
         features = []
-        for i in range(len(r)-1):
-            features.append(r['pixel' + str(i)])
+        for j in range(len(r)-1):
+            features.append(int(r['pixel' + str(j)]))
         point['features'] = features
         data.append(point)
     return data
